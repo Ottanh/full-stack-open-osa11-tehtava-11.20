2202f8e4883ea54745a8a6aef140cd1bd9acd8c8
Olli Tanhuanpää <ottanh@utu.fi>
2021-07-25 12:00:13 +0300

    tehtävät 3.15-3.18

--- src/index.js
+++ src/index.js
@@ -1,48 +1,36 @@
-require('dotenv').config()
 
+require('dotenv').config()
+const Person = require('../models/person')
 const express = require('express')
 const app = express()
-app.use(express.json())
+const cors = require('cors')
 
 const morgan = require('morgan');
 morgan.token('body', req => {
   return JSON.stringify(req.body)
 })
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-const cors = require('cors')
-app.use(cors())
+
 
 app.use(express.static('build'))
+app.use(express.json())
+app.use(cors())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 
-const Person = require('../models/person')
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
 
-let persons = [
-    {
-      "name": "Arto Hellas",
-      "number": "050323666969",
-      "id": 1
-    },
-    {
-      "name": "Dan Abramov",
-      "number": "12-43-234345",
-      "id": 3
-    },
-    {
-      "name": "Mary Poppendieck",
-      "number": "39-23-6423122",
-      "id": 4
-    },
-    {
-      "name": "Ada Lovelace",
-      "number": "020202420",
-      "id": 5
-    }
-  ]
+  next(error)
+}
+
 
 app.get('/info', (req, res) => {
+
   const date = new Date()
   Person.find({}).countDocuments().then(c => {
     res.send(
@@ -54,28 +42,33 @@ app.get('/info', (req, res) => {
 })
 
 app.get('/api/persons', (request, response) => {
+
   Person.find({}).then(persons => {
     response.json(persons)
   })
 })
 
-app.get('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  const person = persons.find(person => person.id === id)
+app.get('/api/persons/:id', (request, response, next) => {
   
-  if (person) {
-    response.json(person)  
-  } else {
-    response.status(404).end()
-  }
-
+  Person.findById(request.params.id)
+    .then(person => {
+      if (person) {
+        response.json(person)  
+      } else {
+        response.status(404).end()
+      }
+    })
+    .catch(error => next(error))
+  
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = Number(request.params.id)
-  persons = persons.filter(person => person.id !== id)
+app.delete('/api/persons/:id', (request, response, next) => {
 
-  response.status(204).end()
+  Person.findByIdAndRemove(request.params.id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 const generateID = (max) => {
@@ -92,13 +85,6 @@ app.post('/api/persons', (request, response) => {
     })
   }
 
-  /*
-  if (persons.find(person => person.name === body.name)) {
-    return response.status(400).json({ 
-      error: 'name is already in the phonebook' 
-    })
-  }*/
-
   const person =  new Person({
     name: body.name,
     number: body.number
@@ -108,10 +94,25 @@ app.post('/api/persons', (request, response) => {
     response.json(savedPerson)
   })
 
-  //persons = persons.concat(person)
-  //response.json(persons)
 })
 
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const body = request.body
+
+  const person =  {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, person, { new: true })
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
+})
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
