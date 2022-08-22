b322a51ecaa09d58047df17be66fa56ea342bc37
Olli Tanhuanpää <ottanh@utu.fi>
2022-08-22 18:38:23 +0300

    initial commit

--- controllers/persons.js
+++ controllers/persons.js
@@ -1,39 +1,8 @@
 
-require('dotenv').config()
+const personsRoputer = require('express').Router()
 const Person = require('../models/person')
-const express = require('express')
-const app = express()
-const cors = require('cors')
 
-
-const morgan = require('morgan')
-morgan.token('body', req => {
-  return JSON.stringify(req.body)
-})
-
-
-
-app.use(express.static('build'))
-app.use(express.json())
-app.use(cors())
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
-
-
-const errorHandler = (error, request, response, next) => {
-  console.error(error.message)
-
-  if (error.name === 'CastError') {
-    return response.status(400).send({ error: 'malformatted id' })
-  }
-  else if (error.name === 'ValidationError') {
-    return response.status(400).json({ error: error.message })
-  }
-
-  next(error)
-}
-
-
-app.get('/info', (req, res) => {
+personsRoputer.get('/info', (req, res) => {
 
   const date = new Date()
   Person.find({}).countDocuments().then(c => {
@@ -45,14 +14,14 @@ app.get('/info', (req, res) => {
 
 })
 
-app.get('/api/persons', (request, response) => {
+personsRoputer.get('/', (request, response) => {
 
   Person.find({}).then(persons => {
     response.json(persons)
   })
 })
 
-app.get('/api/persons/:id', (request, response, next) => {
+personsRoputer.get('/:id', (request, response, next) => {
 
   Person.findById(request.params.id)
     .then(person => {
@@ -66,7 +35,7 @@ app.get('/api/persons/:id', (request, response, next) => {
 
 })
 
-app.delete('/api/persons/:id', (request, response, next) => {
+personsRoputer.delete('/:id', (request, response, next) => {
 
   Person.findByIdAndRemove(request.params.id)
     .then(() => {
@@ -81,12 +50,12 @@ const generateID = (max) => {
 }
 */
 
-app.post('/api/persons', (request, response, next) => {
+personsRoputer.post('/', (request, response, next) => {
   const body = request.body
 
   if (!body.name || !body.number) {
     return response.status(400).json({
-      error: 'name or number missing'
+      error: 'title or url missing'
     })
   }
 
@@ -104,7 +73,7 @@ app.post('/api/persons', (request, response, next) => {
 })
 
 
-app.put('/api/persons/:id', (request, response, next) => {
+personsRoputer.put('/:id', (request, response, next) => {
   const body = request.body
 
   const person = {
@@ -119,9 +88,4 @@ app.put('/api/persons/:id', (request, response, next) => {
     .catch(error => next(error))
 })
 
-app.use(errorHandler)
-
-const PORT = process.env.PORT || 3001
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
-})
+module.exports = personsRoputer
